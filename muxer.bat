df22d55f3bc6060fff7707478d46570249effdd9
Dom Slee <domslee97@gmail.com>
2015-11-22 16:30:38 +1100

    * Fixed universal batch muxer

--- muxer.bat
+++ muxer.bat
@@ -1,8 +1,10 @@
+@echo off
 echo YouTube Downloader Muxer
 echo -----------------------------------------------
 echo This application merges *.m4v, *.webm video with *.m4a audio into either *.mp4 or *.avi
 echo.
 echo.
+setlocal enabledelayedexpansion
 
 set muxCount=0
 for %%i in (*.m4a) do (
@@ -11,22 +13,22 @@ for %%i in (*.m4a) do (
 	for %%j in (*.m4v) do (
         set vTitle=%%j
 		set vTitleStrip=%%~nj
-		if "%vTitleStrip%"=="%titleStrip%" (
+		if "%%~ni"=="%%~nj" (
 			if NOT "%vTitle%"=="*.m4v" (
                 set ext=mp4
 				call :ffmpegcall
-				set muxCount=%muxCount%+1
+				set /A muxCount=!muxCount!+1
 			)
 		)
 	)
-	for %%j in (*.webm) do (
-		set vTitle=%%j
-        set vTitleStrip=%%~nj
-		if "%vTitleStrip%"=="%titleStrip%" (
+	for %%k in (*.webm) do (
+		set vTitle=%%k
+        set vTitleStrip=%%~nk
+		if "%%~ni"=="%%~nk" (
 			if NOT "%vTitle%"=="*.webm" (
 				set ext=avi
 				call :ffmpegcall
-				set muxCount=%muxCount%+1
+				set /A muxCount=!muxCount!+1
 			)
 		)
 	)
@@ -41,21 +43,14 @@ goto EOF
 :ffmpegcall
 echo.
 echo.
-ffmpeg -i "%vTitle%" -i "%title%" -vcodec copy -acodec copy "%titleStrip%.%ext%"
-echo %errorlevel%
-if %errorlevel%==1 (
-	echo ded
-	echo ERROR occurred when trying to mux using FFmpeg. This probably means that you don't have FFmpeg installed
+ffmpeg -i "%vTitle%" -i "%title%" -vcodec copy -acodec copy "%titleStrip%.%ext%" >nul
+if %errorlevel% GTR 0 (
+	echo ERROR occurred when trying to mux using FFmpeg.
 ) else (
-	echo ded
-
+	echo "%titleStrip%.%ext%" muxed successfully
 	del "%title%"
 	del "%vTitle%"
-	echo SUCCESS! Made %titleStrip%
 )
 exit /b
-
 :EOF
-pause
-pause
-pause
+pause >nul
